d77c95b7703036fb2691680febc969a4b069ca09
Jon Fei <jhfernan@mail.usf.edu>
2022-01-11 11:11:46 +0800

    Initial Commit

---
+++ Shared/BurrowDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurrowDesignApp.swift
+//  Shared
+//
+//  Created by Jonathan Fernandes on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BurrowDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
